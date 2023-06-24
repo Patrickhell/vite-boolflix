a6069092697bdf34bc03c7dd76aaa2f694671c96
Patrickhell <patrickhell19@yahoo.fr>
2023-06-24 21:33:46 +0200

    implementazione effetto over sui diversi poster

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -1,63 +1,78 @@
 
 <template>
-    <SearchFilmsTv @searched ="RequestSend"/>
-    <div class="films">
-        <div  v-for= "elements in filmsList" :key ="elements">
-            <img :src="generateUrlImage(elements.poster_path)" alt="path_imageFilm"> 
-            <p><strong> FILM Title :</strong> {{elements.title}} </p>    
-            <p><strong>ORIGINAL Title FILM :</strong>{{elements.original_title}}</p> 
-            <div>
-                <span><strong> FILM Language :</strong></span>
-                <img  :src="getFlag(elements.original_language)" alt="FLAG IMAGE NOT FOUND" 
-                style="width: 30px; color: brown;">
-            </div> 
-            <div class="d-flex">
-                <p>
-                    <strong> Film Vote Average :</strong>
-            
-                </p>
-                <div v-for="n in 10" :key="n">
-                    <i  class ="fa-star" :class="( Math.trunc(Math.ceil(elements.vote_average))) ?'fa-solid' : 'fa-regular'"></i> 
+    <main >
+        <SearchFilmsTv @searched ="RequestSend"/>
+        <div class="container pt-5 mb-5" >
+            <div class="col-12">
+                <h5 style="color:antiquewhite">FILMS NETFLIX</h5>
+                <div class="row cards-content">
+                    <div class="card col-2 p-0" v-for= "elements in filmsList" :key ="elements" >
+                        <img :src="generateUrlImage(elements.poster_path)" alt="path_imageFilm" class="poster p-0" >
+                        <div class="bodyCard p-2">
+                            <p ><span> Titolo: </span>  {{elements.title}} </p>    
+                            <p> <span> Titolo Originale:</span> {{elements.original_title}}</p> 
+                            <div>
+                                <span> Lingua Originale: </span>
+                                <img  :src="getFlag(elements.original_language)" alt=" Flag image not found" 
+                                style="width: 30px; color: brown;"> 
+                            </div>
+                           
+                            <div class="d-flex">
+                                <span> Vote:</span>
+                                <div v-for="n in 5" :key="n">
+                                  <i  class ="fa-star" :class="(n <= Math.trunc(Math.ceil(elements.vote_average/2))) ?'fa-solid' : 'fa-regular'"></i> 
+                                </div>
+                            </div>
+                            <p>
+                                <span>Overview:</span>
+                                {{ elements.overview }}
+                            </p>
+
+                        </div>
+                    </div>
                 </div>
-            
             </div>
         </div>
-
-        <div v-for= "seriesTv in tvList" :key ="seriesTv">
-            <img :src="generateUrlImage(seriesTv.poster_path)" alt="path_imageSerie"> 
-            <p><strong>Serie Title :</strong> {{seriesTv.name}} </p>    
-            <p><strong>ORIGINAL Title Serie :</strong>{{seriesTv.original_name}}</p> 
-            <div>
-                <span><strong> SERIE Language  :</strong></span>
-                <img  :src="getFlag(seriesTv.original_language)" alt="FLAG IMAGE NOT FOUND" 
-                style="width: 30px; color: brown;">
-            </div> 
-            
-                
-
-            <div class="d-flex">
-                <p>
-                    <strong> Serie Vote Average :</strong>
-            
-                </p>
-                <div v-for="n in 5" :key="n">
-                    <i  class ="fa-star" :class="( n <= Math.trunc(Math.ceil(seriesTv.vote_average/2)))? 'fa-solid':'fa-regular'"></i> 
+        <div class="container  " >
+            <div class=" col-12 ">
+                <h5 style="color:antiquewhite">SERIES NETFLIX</h5>
+                <div class="row cards-content" >
+                    <div class="card col-2 p-0" v-for= "seriesTv in tvList" :key ="seriesTv" >
+                        <img :src="generateUrlImage(seriesTv.poster_path)" alt="path_imageSerie" class="poster p-0" > 
+                        <div class="bodyCard p-2 ">
+                            <p><span class="semibold"> Titolo :</span> {{seriesTv.name}}</p>    
+                            <p><span> Titolo Originale:</span> {{seriesTv.original_name}}</p> 
+                            <div>
+                                <span> Lingua Originale:</span>
+                                <img  :src="getFlag(seriesTv.original_language)" alt=" Flag image not found" 
+                                style="width: 30px; color: brown;">
+                            </div>
+                           
+                            <div class="d-flex">
+                                <span > Vote:</span>
+                                <div v-for="n in 5" :key="n">
+                                  <i  class ="fa-star" :class="( n <= Math.trunc(Math.ceil(seriesTv.vote_average/2)))? 'fa-solid':'fa-regular'"></i> 
+                                </div>
+                            </div>
+                            <p>
+                                <span >Overview:</span>
+                                {{ seriesTv.overview }}
+                            </p>
+                        </div>
+                    </div>
                 </div>
-            
             </div>
         </div>
-        
+    
+    </main>
+    
+    
 
-    </div>
-       
-        
 
-       
-        
+    
 </template>
 
 <script>
-
 import SearchFilmsTv from './SearchFilmsTv.vue';
 import axios from 'axios';
 
@@ -115,7 +130,7 @@ export default {
 
         generateUrlImage(Pathposter) {
 
-            let partialUrl = 'https://image.tmdb.org/t/p/w185' + Pathposter;
+            let partialUrl = 'https://image.tmdb.org/t/p/w342' + Pathposter;
             return partialUrl
 
         },
@@ -135,8 +150,80 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+main{
+    background-color:#141922
+}
+
+.container{
+    max-width: 1220px;
+    margin: 0 auto;
+    
+}
+
+
 ul{
     list-style-type: none;
 }
+
+
+.card{
+    background-color:black;
+    text-align: left;
+    position: relative;
+    overflow-y:auto ;
+    
+
+}
+
+div img.poster{
+    object-fit: cover;
+     width: 100%;
+     opacity: .7;
+     height: auto;
+    
+
+   
+}
+span{
+    font-size: 14px;
+    color: white;
+}
+p{
+    font-size: 12px;
+    color: antiquewhite;
+    
+
+}
+.fa-solid{
+    color: goldenrod;
+    font-size: 15px;
+
+}
+.fa-regular{
+    color:aliceblue
+}
+.bodyCard{
+    display: none;
+   
+}
+div.card:hover .bodyCard{
+    display: block;
+    position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 1;
+ 
+}
+div.card:hover .poster{
+    display: none;
+}
+
+
+
+
 
 </style>
--- src/components/SearchFilmsTv.vue
+++ src/components/SearchFilmsTv.vue
@@ -1,16 +1,25 @@
 <template>
-    <div class="d-flex justify-content-center">
-        <div class="input-group my-4 mx-4 " style="width: 600px;">
-          <input type="text" class="form-control" placeholder="Search Film" aria-label="Search Films o Series" aria-describedby="button-addon2" v-model="SearchText">
-          <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="$emit('searched',SearchText)">VAI</button>
+    <header>
+        <div class="col-10">
+            <div class="row justify-content-around ">
+              <div class="col-3 m-4 ">
+                 <img src="https://image.tmdb.org/t/p/w92/wwemzKWzjKYJFfCeiB57q3r4Bcm.png" alt="">
+              </div>
+              <div class=" col-7 m-4 ">
+                <div class="input-group " >
+                    <input type="text" class="form-control" placeholder="Search Films o Series" aria-label="Search Films o Series" aria-describedby="button-addon2" v-model="SearchText" style="background-color: black; color: white;border-color:#282828 ;">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="$emit('searched',SearchText)">Search</button>
+                </div>
+              </div>
+            </div>
+
         </div>
-    </div>
+    </header>
     
-    
- </template>
+</template>
  
  <script>
- 
+
  export default {
      name: 'SearchFilmsTv',
      
@@ -24,12 +33,20 @@
          logContent(content) {
              console.log(content)
          }
-     }
- 
+     },
+
  }
  </script>
  
- <style lang="scss">
+ <style lang="scss" scoped>
+ header{
+    height:80px ;
+    margin: 0 auto;
+    background-color:#282828;
+    opacity: .6;
+
+
+ }
  
  </style>
  
